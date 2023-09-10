cd3c6e69b1281da9fe0c378e0f06de73628b2b13
eduardonunespp <carlospn200406@gmail.com>
2023-09-10 09:26:15 -0300

    feat: add button component

---
+++ src/app/shared/components/ld-button/ld-button.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LdButtonComponent } from './ld-button.component';
+
+describe('LdButtonComponent', () => {
+  let component: LdButtonComponent;
+  let fixture: ComponentFixture<LdButtonComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LdButtonComponent]
+    });
+    fixture = TestBed.createComponent(LdButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
