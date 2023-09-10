030394e05a2aae85a16d529e517a1a0677959f90
eduardonunespp <carlospn200406@gmail.com>
2023-09-10 19:05:53 -0300

    refactor: input angular material

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -19,7 +19,7 @@ export class RegisterComponent implements OnInit {
     role: ['', [Validators.required ]],
     fullName: ['', [Validators.required]],
     birthdate: ['', [Validators.required]],
-    email: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]]
   })
 
