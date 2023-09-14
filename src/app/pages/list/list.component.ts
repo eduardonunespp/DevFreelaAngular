25ef72abb67060dccf63a8f69c20c4cc92f99258
Carlos Eduardo <carlospn200406@gmail.com>
2023-09-14 17:09:27 -0300

    feat: add functions

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -10,39 +10,37 @@ import { ListService } from './service/list.service';
 })
 export class ListComponent implements OnInit {
   list: ListItem[] = [];
-  products: string = 'w';
+
+  isLoadingTable: boolean = true;
 
   constructor(private listService: ListService) {}
 
   ngOnInit(): void {
-    // window.onload = function () {
-    //   document.querySelector('#name').innerText =
-    //     localStorage.getItem('userName');
-    //   document.querySelector('#role').innerText = localStorage.getItem('role');
-
-    // };
     this.getProjects();
   }
 
-  getProjects() {
-    this.listService.getList().subscribe((response) => {
-      console.log(response);
-      this.list = response;
+  goToEdit(id: string | any) {
+    window.location.href = `project-create-edit.html?id=${id}`;
+  }
+
+  deleteProject(id: string | any) {
+    this.listService.deleteProject(id).subscribe((response) => {
+      this.list = this.list.filter((project) => project.id != id);
+
       this.buildTable();
     });
   }
 
-  // getProjects() {
-  //   this.listService.getList()
-  //     .then((response) => response.json())
-  //     .then((response: ListItem[]) => {
-  //       this.list = response
-  //       this.buildTable()
-  //     });
-  // }
+  getProjects() {
+    this.listService.getProjects().subscribe((response) => {
+      console.log(response);
+      this.list = response;
+      this.buildTable();
+      this.isLoadingTable = false;
+    });
+  }
 
   buildTable() {
-    (document.querySelector('#table-body') as any).innerHTML = '';
     const idClient = localStorage.getItem('idClient');
 
     console.log(idClient);
@@ -51,25 +49,25 @@ export class ListComponent implements OnInit {
       (listItem: ListItem) => listItem.idClient.toString() === idClient
     );
 
-    this.list.forEach((listItem) => {
-      let template = `
-            <div class="row">
-                <div class="title-description">
-                    <h6 class="title">${listItem.title}</h6>
-                    <p class="description">${listItem.description}</p>
-                </div>
-                <div class="price">R$ ${listItem.totalCost}</div>
-                <div class="actions">
-                    <span class="edit material-icons" onclick="goToEdit(${listItem.id})">edit</span>
-                    <span class="delete material-icons" onclick="deleteProject(${listItem.id})">delete_outline</span>
-                </div>
-            </div>
-        `;
+    // this.list.forEach((listItem) => {
+    //   let template = `
+    //         <div class="row">
+    //             <div class="title-description">
+    //                 <h6 class="title">${listItem.title}</h6>
+    //                 <p class="description">${listItem.description}</p>
+    //             </div>
+    //             <div class="price">R$ ${listItem.totalCost}</div>
+    //             <div class="actions">
+    //                 <span class="edit material-icons" onclick="goToEdit(${listItem.id})">edit</span>
+    //                 <span class="delete material-icons" onclick="deleteProject(${listItem.id})">delete_outline</span>
+    //             </div>
+    //         </div>
+    //     `;
 
-      (document.querySelector('#table-body') as any).insertAdjacentHTML(
-        'beforeend',
-        template
-      );
-    });
+    //   (document.querySelector('#table-body') as any).insertAdjacentHTML(
+    //     'beforeend',
+    //     template
+    //   );
+    // });
   }
 }
